42e8c2a6aa857a5b36dd5d51ce136f5b33c582a8
jack272hughes <jack.hughes2@sky.uk>
2022-06-04 20:50:04 +0100

    Implement seat selection controller for handling selection of seats
    Added a SeatDisplay class for printing current seat layout

---
+++ src/main/java/ots/controllers/SeatSelectionController.java
@@ -0,0 +1,81 @@
+package ots.controllers;
+
+import ots.database.DatabaseController;
+import ots.models.Event;
+import ots.models.seat.RowMap;
+import ots.models.seat.Seat;
+import ots.models.seat.SeatState;
+import ots.utils.SeatDisplay;
+
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class SeatSelectionController {
+    private static final int MAXIMUM_SELECTED_SEATS = 5;
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final Pattern isPossibleSeat = Pattern.compile("^[A-H][1-8]$");
+
+    private final DatabaseController databaseController;
+
+    public SeatSelectionController(DatabaseController databaseController) {
+        this.databaseController = databaseController;
+    }
+
+    public List<Seat> startSeatSelection(Event event) {
+        List<Seat> selectedSeats = new ArrayList<>();
+
+        String choice = "";
+        while (!"purchase".equalsIgnoreCase(choice) && !"exit".equalsIgnoreCase(choice)) {
+            RowMap seats = databaseController.getSeats().getOrDefault(event.getId(), RowMap.empty());
+            new SeatDisplay(seats, selectedSeats).display();
+            System.out.println("Selected seats: " + selectedSeats.stream().map(Seat::getLocation).collect(Collectors.joining(", ")));
+            System.out.println("Please select a row and column (e.g. A1) or enter 'purchase' or 'exit':");
+            choice = scanner.nextLine().strip();
+
+            Optional<Seat> optionalChosenSeat = validateChoice(event, choice);
+
+            if (optionalChosenSeat.isEmpty()) {
+                System.out.println(String.format("The entered value '%s' is not a valid seat", choice));
+                continue;
+            }
+
+            Seat chosenSeat = optionalChosenSeat.get();
+            if (chosenSeat.getState().equals(SeatState.HOLD) && selectedSeats.stream().anyMatch(seat -> seat.equals(chosenSeat))) {
+                databaseController.updateSeatState(event, chosenSeat, SeatState.AVAILABLE);
+                selectedSeats.removeIf(seat -> seat.equals(chosenSeat));
+                System.out.println(String.format("The seat at '%s' has been unselected", choice));
+            } else if (chosenSeat.getState().equals(SeatState.AVAILABLE)) {
+                if (selectedSeats.size() >= MAXIMUM_SELECTED_SEATS) {
+                    System.out.println(String.format("You have reached the maximum number of %s selected seats", MAXIMUM_SELECTED_SEATS));
+                } else {
+                    databaseController.updateSeatState(event, chosenSeat, SeatState.HOLD);
+                    selectedSeats.add(chosenSeat);
+                    System.out.println(String.format("The seat at '%s' has been selected", choice));
+                }
+            } else {
+                System.out.println(String.format("The seat at '%s' is currently not available", choice));
+            }
+        }
+
+        if ("exit".equalsIgnoreCase(choice)) {
+            selectedSeats.forEach(seat -> databaseController.updateSeatState(event, seat, SeatState.AVAILABLE));
+            return Collections.emptyList();
+        }
+
+        return selectedSeats;
+    }
+
+    private Optional<Seat> validateChoice(Event event, String choice) {
+        if (!isPossibleSeat.matcher(choice).find()) {
+            return Optional.empty();
+        }
+
+        String[] rowColumnPair = choice.split("");
+        String row = rowColumnPair[0];
+        int column = Integer.parseInt(rowColumnPair[1]);
+
+        Seat seat = databaseController.getSeat(event, row, column);
+        return Optional.of(seat);
+    }
+}
---
+++ src/main/java/ots/utils/SeatDisplay.java
@@ -0,0 +1,67 @@
+package ots.utils;
+
+import ots.models.seat.ColumnMap;
+import ots.models.seat.RowMap;
+import ots.models.seat.Seat;
+import ots.models.seat.SeatState;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class SeatDisplay {
+    private static final String COLOUR_RESET = "\033[0m";
+    private static final String GREEN = "\033[0;32m";
+    private static final String CYAN = "\033[0;36m";
+    private static final String WHITE = "\033[0;37m";
+    private static final String RED = "\033[0;31m";
+
+    private final RowMap rows;
+    private final HashSet<String> selectedSeats = new HashSet<>();
+
+    public SeatDisplay(RowMap rows, List<Seat> selectedSeats) {
+        this.rows = rows;
+        selectedSeats.forEach(seat -> this.selectedSeats.add(seat.getLocation()));
+    }
+
+    public void display() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("\n    | 1  | 2  | 3  | 4  | 5  | 6  | 7  | 8  |\n")
+                     .append("---------------------------------------------");
+        for (int rowNum = 1; rowNum <= 8; rowNum++) {
+            String rowLetter = Character.toString(rowNum + 64);
+            stringBuilder.append("\n| ").append(rowLetter).append(" |");
+
+            ColumnMap columnMap = rows.getColumnMap(rowLetter);
+            for (int columnNum = 1; columnNum <= 8; columnNum++) {
+                SeatState seatState = columnMap.getSeatState(columnNum);
+                String colouredSeat = getColouredSeat(seatState, rowLetter + columnNum);
+                stringBuilder.append(" ").append(colouredSeat).append(" |");
+            }
+        }
+
+        System.out.println(stringBuilder.toString());
+    }
+
+    private String getColouredSeat(SeatState seatState, String location) {
+        String colouredSeat;
+
+        switch (seatState) {
+            case AVAILABLE:
+                colouredSeat = GREEN;
+                break;
+            case HOLD:
+                if (selectedSeats.contains(location)) {
+                    colouredSeat = CYAN;
+                } else {
+                    colouredSeat = WHITE;
+                }
+                break;
+            case RESERVED:
+            default:
+                colouredSeat = RED;
+        }
+
+        colouredSeat += "██" + COLOUR_RESET;
+        return colouredSeat;
+    }
+}
